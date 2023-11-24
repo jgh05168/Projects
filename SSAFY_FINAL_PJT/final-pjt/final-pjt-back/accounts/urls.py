ddb1a3548c2d2d422a303a8976066fd7e9d95374
gyuhun <jgh05168@gmail.com>
2023-11-24 13:48:53 +0900

    ssafy_관통pjt_Final

--- SSAFY_FINAL_PJT/final-pjt/final-pjt-back/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('signout/', views.signout),
-    path('profile/update/<int:user_pk>/', views.profileUpdate),
-    path('profile/<int:user_pk>', views.user_list),
-]
